9fe551286f3ccb5d5431347e17bd6cd80fbf84cd
Josoa <josoavonjiniaina13@gmail.com>
2025-03-16 15:16:32 +0300

    ref: Signalement

--- Contrat/crudDatabaseContrat.py
+++ Contrat/crudDatabaseContrat.py
@@ -6,6 +6,11 @@ from traitement import typestraitement, creation_traitement, obtenir_types_trait
 from planning import create_planning, obtenir_redondances, read_planning, update_planning, delete_planning
 from facture import create_facture, read_facture, obtenir_axe_contrat, delete_facture, update_facture
 
+"""
+  **** Remarque: l'historique présent dans ce fichier est obsolète
+  Veuillez vous reférer au fichier historique.py pour le code fonctionnel
+"""
+
 # Accès aux catégories
 async def obtenir_categories(pool, table_name, column_name):
     async with pool.acquire() as conn:
@@ -17,33 +22,6 @@ async def obtenir_categories(pool, table_name, column_name):
                 return enum_str
             else:
                 return []
-
-# Pour avancement
-async def create_avancement(pool, traitement_id, motif, type_avancement):
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("INSERT INTO Avancement (traitement_id, motif, type) VALUES (%s, %s, %s)", (traitement_id, motif, type_avancement))
-            await conn.commit()
-            return cur.lastrowid
-
-async def read_avancement(pool, avancement_id):
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("SELECT * FROM Avancement WHERE avancement_id = %s", (avancement_id,))
-            return await cur.fetchone()
-
-async def update_avancement(pool, avancement_id, traitement_id, motif, type_avancement):
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("UPDATE Avancement SET traitement_id = %s, motif = %s, type = %s WHERE avancement_id = %s", (traitement_id, motif, type_avancement, avancement_id))
-            await conn.commit()
-
-async def delete_avancement(pool, avancement_id):
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("DELETE FROM Avancement WHERE avancement_id = %s", (avancement_id,))
-            await conn.commit()
-
 
 async def main():
     # Connexion à la base de données
@@ -101,7 +79,7 @@ async def main():
 
         choix_table = input("Choisissez une table (1-7) : ")
 
-        if choix_table == '8':
+        if choix_table == '7':
             break
 
         table_functions = {

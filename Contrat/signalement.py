6da98ee781c40554486c35b64e31692f99719bbf
Josoa <josoavonjiniaina13@gmail.com>
2025-03-16 16:13:12 +0300

    fix: Correction du signalement

--- Contrat/signalement.py
+++ Contrat/signalement.py
@@ -6,10 +6,18 @@ async def create_signalement(pool, planning_detail_id, motif, type_signalement):
             await conn.commit()
             return cur.lastrowid
 
-async def read_signalement(pool, avancement_id):
+async def read_signalement(pool, signalement_id):
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("SELECT * FROM Avancement WHERE avancement_id = %s", (avancement_id,))
+            await cur.execute("""
+                SELECT s.signalement_id, s.motif, s.type,
+                       pd.planning_detail_id, pd.traitement_id, pd.date_debut, pd.date_fin,
+                       t.type_traitement
+                FROM Signalement s
+                JOIN PlanningDetails pd ON s.planning_detail_id = pd.planning_detail_id
+                JOIN Traitement t ON pd.traitement_id = t.traitement_id
+                WHERE s.signalement_id = %s
+            """, (signalement_id,))
             return await cur.fetchone()
 
 async def update_signalement(pool, signalement_id, planning_detail_id, motif, type_signalement):
@@ -18,9 +26,8 @@ async def update_signalement(pool, signalement_id, planning_detail_id, motif, ty
             await cur.execute("UPDATE Signalement SET planning_detail_id = %s, motif = %s, type = %s WHERE signalement_id = %s", (planning_detail_id, motif, type_signalement, signalement_id))
             await conn.commit()
 
-async def delete_signalement(pool, avancement_id):
+async def delete_signalement(pool, signalement_id):
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("DELETE FROM Avancement WHERE avancement_id = %s", (avancement_id,))
+            await cur.execute("DELETE FROM Signalement WHERE signalement_id = %s", (signalement_id,))
             await conn.commit()
-

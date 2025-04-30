cbf4d28def5f522e8655a898ff5a58fb624e938b
Josoa <josoavonjiniaina13@gmail.com>
2025-04-30 13:36:54 +0300

    Update Planificator.sql

--- scriptSQL/Planificator.sql
+++ scriptSQL/Planificator.sql
@@ -4,20 +4,6 @@
     Veuillez vous réferer à la documentation ou envoyer un mail à l'auteur si vous avez besoin d'aide
 */
 
--- Effacer une table dans Planificator
-DROP TABLE IF EXISTS Account;
-DROP TABLE IF EXISTS Contrat;
-DROP TABLE IF EXISTS Traitement;
-DROP TABLE IF EXISTS TypeTraitement;
-DROP TABLE IF EXISTS Client;
-DROP TABLE IF EXISTS Facture;
-DROP TABLE IF EXISTS Historique;
-DROP TABLE IF EXISTS Planning;
-DROP TABLE IF EXISTS PlanningDetails;
-DROP TABLE IF EXISTS Signalement;
-DROP TABLE IF EXISTS Remarque;
-
-
 -- Supprimer complètement la base de données
 DROP DATABASE IF EXISTS Planificator;
 

235839932b34fd4a0e04bf767bf5a922060aa2a4
Josoa <josoavonjiniaina13@gmail.com>
2025-06-11 13:20:17 +0300

    feat: Mise à jour

--- Contrat/fonctionnalités/Excel/connexionDB.py
+++ Contrat/fonctionnalités/Excel/connexionDB.py
@@ -1,9 +1,9 @@
 # Configuration de la base de données
 DB_CONFIG = {
-    'host': 'your_db_host',        # REMPLACEZ PAR VOTRE HÔTE
+    'host': 'localhost',        # REMPLACEZ PAR VOTRE HÔTE
     'port': 3306,
-    'user': 'your_db_user',        # REMPLACEZ PAR VOTRE NOM D'UTILISATEUR
-    'password': 'your_db_password', # REMPLACEZ PAR VOTRE MOT DE PASSE
+    'user': 'sudoted',        # REMPLACEZ PAR VOTRE NOM D'UTILISATEUR
+    'password': '100805Josh', # REMPLACEZ PAR VOTRE MOT DE PASSE
     'db': 'Planificator',
     'autocommit': True
 }

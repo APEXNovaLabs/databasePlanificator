6e5aceb82bf2ca2006f7d708740e9e75d96cf729
Josoa <josoavonjiniaina13@gmail.com>
2025-06-11 13:04:06 +0300

    feat: script de connexion à la base de données

---
+++ Contrat/fonctionnalités/Excel/connexionDB.py
@@ -0,0 +1,9 @@
+# Configuration de la base de données
+DB_CONFIG = {
+    'host': 'your_db_host',        # REMPLACEZ PAR VOTRE HÔTE
+    'port': 3306,
+    'user': 'your_db_user',        # REMPLACEZ PAR VOTRE NOM D'UTILISATEUR
+    'password': 'your_db_password', # REMPLACEZ PAR VOTRE MOT DE PASSE
+    'db': 'Planificator',
+    'autocommit': True
+}
